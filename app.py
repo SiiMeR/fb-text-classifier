46576193b34a4b879ea12df88506df01437f2be7
Siim Raudsepp <siimraudsepp@msn.com>
2017-12-28 22:33:09 +0200

    Added facebook webhook

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask, request, redirect, url_for
 from src.classifier import MyTextClassifier
 from werkzeug.utils import secure_filename
 import os
+import json
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = APP_ROOT + "/src/web/files"
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 clf = None
-
 
 
 @app.route('/hi/<name>')
@@ -21,6 +21,7 @@ def say_hi(name):
 def findauthor(text):
     print([text])
     return clf.predictAuthor([text])[0]
+
 
 @app.route('/upload/', methods=['POST'])
 def upload_file():
@@ -46,6 +47,74 @@ def upload_file():
             return "File upload success"
     return "Done"
 
+
+
+# https://github.com/hartleybrody/fb-messenger-bot
+@app.route('/', methods=['GET'])
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+
+    return "Hello world", 200
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+
+    # endpoint for processing incoming messaging events
+
+    data = request.get_json()
+    print(data)
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    send_message(sender_id, "roger that!")
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+
+    print("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        print(r.status_code)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
