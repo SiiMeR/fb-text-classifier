4c43b082bba492a1a36f30943f261cd1627d471b
Siim Raudsepp <siimraudsepp@msn.com>
2017-12-28 19:50:49 +0200

    API upload file works somewhat

--- src/web/api.py
+++ src/web/api.py
@@ -1,11 +1,13 @@
 import requests
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from src.classifier import MyTextClassifier
 from werkzeug.utils import secure_filename
 import os
 
-UPLOAD_FOLDER = 'C:/Users/Kristjan/Desktop/temp'
-
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = APP_ROOT + "\\files"
+print("if " + APP_ROOT)
+print("af " + UPLOAD_FOLDER)
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 clf = None
@@ -19,7 +21,6 @@ def uploadHTML():
         file = request.files['file']
         file.save(file.filename)
 
-        print(file.filename)
 
         global clf
         clf = MyTextClassifier(file)
@@ -39,9 +40,32 @@ def findauthor(text):
     print([text])
     return clf.predictAuthor([text])[0]
 
+@app.route('/upload/', methods=['POST','GET'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file
+
+        if 'file' not in request.files:
+            print('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('No selected file')
+            return redirect(request.url)
+
+        if file:
+            filename = secure_filename(file.filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            print(path)
+            file.save(path)
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return "something"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
