124d5aaefe647a76235227d0da5e1e7a47f43890
Kristjan <kristjan.puusepp@tudeng.ut.ee>
2017-12-28 18:17:30 +0200

    api

--- src/web/api.py
+++ src/web/api.py
@@ -1,10 +1,32 @@
 import requests
 from flask import Flask, request
 from src.classifier import MyTextClassifier
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = 'C:/Users/Kristjan/Desktop/temp'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+clf = None
 
-clf = MyTextClassifier("tekst1.html")
+
+@app.route('/uploadhtml/', methods=['POST'])
+def uploadHTML():
+    if request.method == 'POST':
+        print("Uploading file...")
+
+        file = request.files['file']
+        file.save(file.filename)
+
+        print(file.filename)
+
+        global clf
+        clf = MyTextClassifier(file)
+        print("File uploaded.")
+        return "Done"
+    else:
+        return "Something went wrong."
 
 
 @app.route('/hi/<name>')
