6991c39763a41f3fa4bbced4d1c0326b0ab61bdd
Kristjan <kristjan.puusepp@tudeng.ut.ee>
2017-12-28 17:33:45 +0200

    api for bot

--- src/web/api.py
+++ src/web/api.py
@@ -1,5 +1,25 @@
 import requests
+from flask import Flask, request
+from src.classifier import MyTextClassifier
 
-r = requests.get('/')
+app = Flask(__name__)
+
+clf = MyTextClassifier("tekst1.html")
+
+
+@app.route('/hi/<name>')
+def say_hi(name):
+    return 'Hello, %s!' % name
+
+
+@app.route('/findauthor/<text>/')
+def findauthor(text):
+    print([text])
+    return clf.predictAuthor([text])[0]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 
